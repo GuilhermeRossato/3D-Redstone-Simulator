e25596858a1100428dc943894ebcea1a36769c94
Guilherme Rossato <guilhermrossato@gmail.com>
2016-12-04 12:17:11 -0200

    Replace threejs with minified version

---
+++ Script/cookie.js
@@ -0,0 +1,39 @@
+// Small snippet to make it easier to use cookies
+
+function setCookie(cname, cvalue, exdays) {
+    if (location.hostname == "") {
+       localStorage.setItem(cname, cvalue);
+    } else {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+d.toUTCString();
+        document.cookie = cname + "=" + cvalue + "; " + expires;
+    }
+}
+
+function getCookie(name) {
+    if (location.hostname == "") {
+        return localStorage.getItem(name);
+    } else {
+        name = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1);
+            if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
+}
