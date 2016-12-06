fbb8665ef8d4546218f52fcd13dfbfb5fea75371
Guilherme Rossato <guilhermrossato@gmail.com>
2016-12-06 15:38:44 -0200

    Syntax Formatting
    mrdoob has a tendency to put lines inbetween lines, I feel like there's
    too many lines to be efficient while editing his classes, so I removed
    them

--- Script/setup.js
+++ Script/setup.js
@@ -9,8 +9,9 @@ domInterface.appendChild(domMenu);
 domInterface.appendChild(domSubmenu);
 var menu = new Menu(domMenu,domSubmenu);
 var logger = new Logger(domInterface);
-if (!('pointerLockElement'in document || 'mozPointerLockElement'in document || 'webkitPointerLockElement'in document)) {
-	logger.error("This experiment will not work correctly.");
+var pointerlockSupport = ('pointerLockElement'in document || 'mozPointerLockElement'in document || 'webkitPointerLockElement'in document);
+if (!pointerlockSupport) {
+	logger.warn("Your controls will adjust accordingly.");
 	logger.warn("Your browser doesn't support pointer lock API.");
 }
 var centerMessage = new CenterMessage(domInterface, "Click to start","[W, A, S, D] to move up, left, down, right\n[Numeric Keys] to change selected block\n[E, ESC, I] to change selected tool (top menu)\n[Ctrl + 1, 2, 3] Also changes the selected tool\n[Ctrl + C, Ctrl + V] copy and paste selection\n[ +, - ] to change tools menu size");
