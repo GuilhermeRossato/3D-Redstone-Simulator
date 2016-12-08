c5299e925211c47ee67525d7044308737f539ce6
Guilherme Rossato <guilhermrossato@gmail.com>
2016-12-08 08:32:38 -0200

    MeshController Base
    This class will allow us to avoid recreating meshes, which decreases the
    amount of data between GPU and CPU to achieve a faster drawing when many
    objects of the same mesh are being drawn

---
+++ Script/Classes/MeshController.js
@@ -0,0 +1,23 @@
+function MeshController() {
+	this.meshes = [];
+}
+MeshController.prototype = {
+	constructor: MeshController,
+	getMesh: function(id) {
+		return this.meshes.filter((obj)=>obj.id === id)[0] || 0;
+	},
+	justAddMesh: function(id, data) {
+		this.meshes.push({
+			id: id
+			data: data
+		});
+	},
+	addMesh: function(id, data) {
+		var obj = this.getMesh(id);
+		if (typeof obj !== "object" && id !== 0) {
+			this.justAddMesh(id, data);
+			return true;
+		}
+		return false;
+	}
+}
