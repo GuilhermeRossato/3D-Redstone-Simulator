2af3cfe85cb59ffbb22d6afb01f1a59365181e88
Guilherme Rossato <guilhermrossato@gmail.com>
2016-12-28 19:20:13 -0200

    Mesh Lookup Process
    Meshes for specific blocks can now be created and loaded whenever
    necessary. It turned out that a class for this process was overkill, so
    it was removed and replaced by an array.

--- Script/Classes/Chunk/ChunkController.js
+++ Script/Classes/Chunk/ChunkController.js
@@ -1,24 +1,105 @@
-function ChunkController() {
+function ChunkController(scene, meshes) {
 	this.blocks = [];
+	this.directedScene = scene;
+	this.meshes = [];
+	this.cubeGeometry = new THREE.BoxGeometry(1,1,1);
+	this.redstoneGeometry = new THREE.BoxGeometry(1,1 / 8,1);
+	this.slabGeometry = new THREE.BoxGeometry(1,0.5,1);
+	this.textureLoader = new THREE.TextureLoader();
 }
 ChunkController.prototype = {
 	constructor: ChunkController,
-	addBlock: function(x, y, z, id) {
+	addBlock: function(x, y, z, id, mesh) {
 		this.blocks.push({
 			x: x,
 			y: y,
 			z: z,
-			id: id
+			id: id,
+			mesh: mesh
 		});
 	},
 	getBlock: function(x, y, z) {
-		return this.blocks.filter((obj)=>obj.x === x && obj.y === y && obj.z === z)[0] || 0;
+		return this.blocks.filter((obj)=>obj.x === x && obj.y === y && obj.z === z)[0];
 	},
 	setBlock: function(x, y, z, id) {
 		var obj = this.getBlock(x, y, z);
-		if (typeof obj === "object")
+		if (typeof obj === "object") {
+			if (id === 0 && obj.id !== id) {} else if (obj.id !== id) {
+				this.scene.remove(obj.mesh);
+			}
 			obj.id === id;
-		else if (id !== 0)
+		} else if (id !== 0) {
 			this.addBlock(x, y, z, id);
+		}
+	},
+	getMesh: function(id) {
+		var mesh = this.meshes.filter((obj)=>obj.id === id)[0];
+		if (mesh == undefined) {
+			logger.warn("Unable to find mesh of id " + id);
+			return undefined;
+		} else {
+			return mesh.mesh;
+		}
+	},
+	createFirstMesh: function(id) {
+		var data = block_data.filter(obj=>obj.id === id)[0];
+		if (data) {
+			var material, texture;
+			if (typeof data.texture === "string") {
+				texture = this.textureLoader.load("Images/Textures/" + data.texture);
+				texture.magFilter = THREE.NearestFilter;
+				texture.minFilter = THREE.LinearMipMapLinearFilter;
+				texture.anisotropy = 0;
+				material = new THREE.MeshLambertMaterial({
+					color: 0x555555,
+					map: texture
+				});
+			} else if (typeof data.texture === "object") {
+				var uniqueTextures = [];
+				[data.texture.top, data.texture.left, data.texture.right, data.texture.front, data.texture.back, data.texture.bottom].forEach(obj=>{
+					if (uniqueTextures.indexOf(obj) === -1)
+						uniqueTextures.push(obj);
+				}
+				);
+				uniqueTextures = uniqueTextures.map(filename=>{
+					texture = this.textureLoader.load("Images/Textures/" + filename);
+					texture.filename = filename;
+					texture.magFilter = THREE.NearestFilter;
+					texture.minFilter = THREE.LinearMipMapLinearFilter;
+					texture.anisotropy = 0;
+					return texture
+				}
+				);
+				material = [];
+				["right", "left", "top", "bottom", "back", "front"].forEach(obj=>{
+					// find the texture of that filename
+					texture = undefined;
+					uniqueTextures.some(object=>{
+						if (object.filename == data.texture[obj]) {
+							texture = object;
+							return true;
+						}
+						return false;
+					}
+					);
+					if (texture) {
+						material.push(new THREE.MeshLambertMaterial({
+							color: 0x555555,
+							map: texture
+						}));
+					} else {
+						logger.error("Unable to find texture " + data[obj] + " in list of unique textures");
+					}
+				}
+				);
+				material = new THREE.MultiMaterial(material);
+			}
+			this.meshes.push({
+				id: id,
+				mesh: material
+			});
+			return true;
+		}
+		return false;
 	}
 }
--- Script/script.js
+++ Script/script.js
@@ -1,16 +1,14 @@
 var options = {
-	playerSpeed: 0.07,
+	playerSpeed: 0.075,
 	viewDistance: 100
 };
 
 var controlsEnabled = false;
-
 var camera, scene, renderer;
 var geometry, material, mesh;
 var controls, raycaster;
-var blocks = [];
 var velocity, light, player;
-
+var chunks;
 var position = {x: 0, y: 0, z: 0}
 
 function setup() {
@@ -21,12 +19,12 @@ function setup() {
 	renderer.setClearColor(0x333333, 1)
 	
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.25, options.viewDistance );
-	camera.position.y = position.x;
-	camera.position.z = position.y;
-	camera.position.x = position.z;
-	//camera.rotation.x = (-Math.PI / 2) * 0.5;
 
 	scene = new THREE.Scene();
+	chunks = new ChunkController(scene);
+	chunks.createFirstMesh(168); // Stone Brick
+	chunks.createFirstMesh(160); // Spruce Plank
+	chunks.createFirstMesh(24); // Sandstone
 	//scene.fog = new THREE.Fog(0xf2f7ff,1,100);
 
 	light = new THREE.DirectionalLight(0xffffff,4);
@@ -37,30 +35,28 @@ function setup() {
 	light.position.set(-4, -2, -3);
 	scene.add(light);
 
-	//light = new THREE.HemisphereLight( 0xeeeeff, 0x777788, 3 );
-	//light.position.set( 0.5, 1, 0.75 );
-	//scene.add( light );
-
 	controls = new THREE.PointerLockControls( camera );
 	player = controls.getObject();
 	loadPlayerFromCookies();
 	scene.add( player );
 
-	raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
+	//raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
 
 	geometry = new THREE.PlaneGeometry( 16*8, 16*8, 10, 10 );
 	geometry.rotateX( - Math.PI / 2 );
 	material = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
 	mesh = new THREE.Mesh( geometry, material );
+	mesh.position.y = -0.5;
 	scene.add( mesh );
 
-	var textureLoader = new THREE.TextureLoader();
-	geometry = new THREE.BoxGeometry(1,1,1);
-	var texture = textureLoader.load( "Images/Textures/stonebrick.png" );
-	texture.magFilter = THREE.NearestFilter;
-	texture.minFilter = THREE.LinearMipMapLinearFilter;
-	texture.anisotropy = 0;
-	material = new THREE.MeshLambertMaterial( { color: 0x555555, map: texture } );
+	
+
+
+
+
+	var textureLoader = chunks.textureLoader;
+	//material = new THREE.MeshLambertMaterial( { color: 0x555555, map: texture } );
+	
 	var translate = [0,0,0];
 	function addBlock(x, y, z, geo, mater) {
 			var cube = new THREE.Mesh(geo,mater);
@@ -68,33 +64,30 @@ function setup() {
 			cube.position.y = y+translate[1];
 			cube.position.z = z+translate[2];
 			scene.add(cube);
-			blocks.push(cube);
 	}
-
+	geometry = chunks.cubeGeometry;
+	material = chunks.getMesh(24);
+	
 	for (var i = -5; i < 6; i ++) {
 		for (var j = -5; j < 6; j ++) {
 			addBlock(i, 0, j, geometry, material);
 		}
 	}
-	texture = textureLoader.load( "Images/Textures/planks_spruce.png" );
-	texture.magFilter = THREE.NearestFilter;
-	texture.minFilter = THREE.LinearMipMapLinearFilter;
-	texture.anisotropy = 0;
-	material = new THREE.MeshLambertMaterial( { color: 0x555555, map: texture } );
-	
+	material = chunks.getMesh(160);
 	translate[0] = -3;
-	addBlock(0, 1, 0, geometry, material);
-	addBlock(1, 2, 0, geometry, material);
-	addBlock(0, 2, 0, geometry, material);
+	addBlock(1, 1, 0, geometry, material);
 	addBlock(2, 2, 0, geometry, material);
-	addBlock(2, 1, 0, geometry, material);
+	addBlock(1, 2, 0, geometry, material);
+	addBlock(3, 2, 0, geometry, material);
+	material = chunks.getMesh(168);
+	//addBlock(2, 1, 0, geometry, material);
 	addBlock(3, 1, 0, geometry, material);
 	addBlock(4, 1, 0, geometry, material);
 	addBlock(4, 1, 1, geometry, material);
 	addBlock(4, 1, 2, geometry, material);
-	addBlock(4, 1, 3, geometry, material);
-
-	texture = textureLoader.load( "Images/Textures/sandstone_top.png" );
+	material = chunks.getMesh(24);
+	translate[0] = 0;
+	addBlock(0, 3, 0, geometry, material);
 	document.body.appendChild(renderer.domElement);
 	velocity = new THREE.Vector3();
 }
@@ -166,6 +159,7 @@ function updatePositionalCookie() {
 		setCookie("rs_posX", player.position.x, 30);
 		setCookie("rs_posY", player.position.y, 30);
 		setCookie("rs_posZ", player.position.z, 30);
+		setCookie("rs_rotX", controls.getPitchObject().rotation.x, 30);
 		setCookie("rs_rotY", player.rotation.y, 30);
 	}
 }
@@ -180,8 +174,10 @@ function loadPlayerFromCookies() {
 			player.position.y = parseFloat(y);
 			player.position.z = parseFloat(z);
 		}
+		x = getCookie("rs_rotX");
 		y = getCookie("rs_rotY");
-		if (y) {
+		if (x&&y) {
+			controls.getPitchObject().rotation.x = parseFloat(x);
 			player.rotation.y = parseFloat(y);
 		}
 	}
