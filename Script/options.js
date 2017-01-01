6a566b1da770a84db7ef8d61268d0d45ef506773
Guilherme Rossato <guilhermrossato@gmail.com>
2017-01-01 18:40:40 -0200

    Options Separation
    List of options is becoming larger and larger, so I might Encapsulate it
    in a class, but for now I'll just have it in a different file

---
+++ Script/options.js
@@ -0,0 +1,49 @@
+var options = {
+	playerSpeed: {
+		horizontal: 0.075,
+		vertical: 0.11
+	},
+	viewDistance: 50,
+	cookiesLastingDays: 40,
+	lights: {
+		instances: [],
+		selected: 0,
+		profiles: [{
+			name: "Darker",
+			definition: [{
+				intensity: 4,
+				position: new THREE.Vector3(3,4,2)
+			}, {
+				intensity: 3.2,
+				position: new THREE.Vector3(-4,-2,-3)
+			}]
+		}, {
+			name: "Contrast",
+			definition: [{
+				intensity: 4.5,
+				position: new THREE.Vector3(3,4,2)
+			}, {
+				intensity: 4,
+				position: new THREE.Vector3(-4,-2,-3)
+			}]
+		}],
+		clearFrom: function(scene) {
+			scene.children.forEach(function(obj) {
+				if (obj instanceof THREE.DirectionalLight) {
+					scene.remove(obj);
+				}
+			})
+		},
+		placeInto: function(scene) {
+			this.profiles[this.selected].definition.forEach(obj=>{
+				let light = new THREE.DirectionalLight(0xffffff,obj.intensity);
+				light.position.copy(obj.position);
+				scene.add(light);
+				this.instances.push(light);
+			}
+			)
+		}
+	},
+	selectionBoundSpace: 1.005
+};
+//setInterval(function() { options.lights.clearFrom(scene); options.lights.placeInto(scene); },1000);
