91e5c6a6335f2b970b510538d841cd064c0f45ee
Guilherme Rossato <guilhermrossato@gmail.com>
2017-01-31 15:19:53 -0200

    Procedural terrain generation
    Completely useless to this project but I wanted to try using perlin
    noise.

--- Script/script.js
+++ Script/script.js
@@ -1,209 +1,139 @@
-var camera, scene, renderer;
-var geometry, material, mesh;
-var controls;
-var velocity, light, player;
-var blocks;
-var animator;
-var selection, collisionHelper;
-var seed;
-
 function manualRandom() {
 	var x = Math.sin(seed++) * 10000;
 	return x - Math.floor(x);
 }
-function generateArea(side) {
-	seed = 9800;
+function setSeed(x, z) {
+	seed = (37 + x + z * 400);
+}
+function getHeightAt(x, z) {
+	return (perlin(x / 30, z / 29) + 1) * 4.5;
+}
+function generateArea(pos, side) {
+	let i, j;
 	var map = new Uint8Array(side * side);
-	var repeatSmoothing = 6;
-	var maxHeight = 25;
-	var i, j, k;
-	for (i = 0; i < side * side; i++)
-		map[i] = manualRandom() * maxHeight;
-	for (k = 0; k < repeatSmoothing; k++) {
-		var newMap = new Uint8Array(side * side);
-		for (i = 0; i < side * side; i++) {
-			var sum = 0;
-			var count = 0;
-			for (j = 0; j < 9; j++) {
-				var realId = i + ((j % 3) - 1) + (((j / 3) | 0) - 1) * side;
-				if (map[realId] !== undefined) {
-					sum += map[realId];
-					count += 1;
-				}
-			}
-			newMap[i] = b(sum / count, map[i], 0);
+	for (i = 0; i < side; i++) {
+		for (j = 0; j < side; j++) {
+			let height = getHeightAt(i+pos.z, j+pos.x);
+			map[i + j * side] = height;
 		}
-		map = newMap;
 	}
-	var min = map[0];
-	for (i = 1; i < side * side; i++)
-		min = Math.min(map[i], min);
-	for (i = 0; i < side * side; i++)
-		map[i] -= min;
 	return map;
 }
-function addBlocksInWorld() {
-	var area = generateArea(20);
-	var candidates = [2, 2, 2, 98, 98, 98, 98, 178, 98, 98, 178, 98, 178, 98, 178, 178, 98, 178, 2, 178, 178, 98, 178, 17, 178, 178];
-	seed = 1302;
-	for (var i = 0; i < 20; i++) {
-		for (var j = 0; j < 20; j++) {
+function removeBlocksFromWorld() {
+	scene.children.forEach(obj => {
+		if (obj.realPosition instanceof THREE.Vector3) {
+			scene.remove(obj);
+		}
+	});
+}
+function addBlocksInWorld(ppos) {
+	/*
+	for (var i = 0 ; i < 3; i++) {
+		for (var j = 0; j < 3; j++) {
+			if (i===0||j===0||i===2||j===2) {
+				for (var k = 0; k < 10+j*Math.random(); k++)
+					blocks.setBlock(i,k,j,17);
+			}
+		}
+	}
+	var size = 4;
+	var pos = {x:ppos.x|0, z:ppos.z|0};
+	var candidates = [2, 2, 2, 98, 98, 98, 178, 98, 98, 178, 98, 178, 98, 178, 178, 98, 178, 2, 178, 178, 98, 178, 17, 178, 178];
+	candidates = candidates.map(obj => (obj===98?24:obj))
+	seed = 48;
+	for (var i = 0; i < size; i++) {
+		for (var j = 0; j < size; j++) {
 			var selectedId = candidates[candidates.length * manualRandom() | 0];
-			blocks.setBlock(i - 10, area[i + j * 20], j - 10, selectedId);
+			blocks.setBlock(i + (pos.x)-size/2, (4+manualRandom()*1.5)|0+(selectedId===17?1:0), j+(pos.z)-size/2, selectedId);
 		}
-	}
+	}*/
 }
-
-function resetLight(scene) {
-	options.lights.clearFrom(scene);
-	options.lights.placeInto(scene);
-}
-function setup() {
-	/* Render Setup */
-	renderer = new THREE.WebGLRenderer({
-		antialias: false,
-		alpha: false
-	});
-	renderer.setClearColor(0x333333, 1);
-	/* Camera Setup*/
-	camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.25,options.viewDistance);
-	document.body.appendChild(renderer.domElement);
-	resize();
-	/* Scene Setup */
-	scene = new THREE.Scene();
-	resetLight(scene);
-	/* Setup Blocks */
-	blocks = new BlockController(scene);
-	addBlocksInWorld();
-	/* Control Setup */
-	controls = new MinecraftControls(scene,camera);
-	/* Base (Bottom Plane) Setup */
-	geometry = new THREE.PlaneGeometry(16 * 4,16 * 4,10,10);
-	geometry.rotateX(-Math.PI / 2);
-	material = new THREE.MeshBasicMaterial({
-		vertexColors: THREE.VertexColors
-	});
-	mesh = new THREE.Mesh(geometry,material);
-	mesh.position.y = -0.5;
-	scene.add(mesh);
-	/* Animation Setup */
-	animator = new AnimationController(camera);
-	blocks.setBlock(0, 0, 0, 2);
-	/* Initialize Variables */
-	raycaster = new THREE.Raycaster(undefined, undefined, 0, 10);
-	velocity = new THREE.Vector3(0,0,0);
-	/* Initializing Selection Box */
-	var h = options.selectionBoundSpace / 2;
-	geometry = new THREE.Geometry();
-	geometry.vertices.push(
-		new THREE.Vector3(-h,-h,-h),
-		new THREE.Vector3(-h,h,-h),
-		new THREE.Vector3(h,h,-h),
-		new THREE.Vector3(h,-h,-h),
-		new THREE.Vector3(-h,-h,-h),
-		new THREE.Vector3(-h,-h,h),
-		new THREE.Vector3(-h,h,h),
-		new THREE.Vector3(h,h,h),
-		new THREE.Vector3(h,-h,h),
-		new THREE.Vector3(h,-h,-h),
-		new THREE.Vector3(h,h,-h),
-		new THREE.Vector3(-h,h,-h),
-		new THREE.Vector3(-h,h,h),
-		new THREE.Vector3(-h,-h,h),
-		new THREE.Vector3(h,-h,h),
-		new THREE.Vector3(h,h,h),
-		new THREE.Vector3(h,h,-h)
-	);
-	material = new THREE.LineBasicMaterial({
-		color: 0x000000
-	});
-	selection = new THREE.Line(geometry, material);
-	scene.add(selection);
-	/* Initializing Collision Helper*/
-	material = new THREE.LineBasicMaterial({
-		color: 0xff0000
-	});
-	geometry = geometry.clone();
-	geometry.vertices.map(obj => obj.multiplyScalar((options.collisionBoundingRect.horizontal+0.0001)/h));
-	collisionHelper = new THREE.Line(geometry, material);
-	scene.add(collisionHelper);
-
-	/* Selection Point */
-	//geometry = new THREE.SphereGeometry( 0.1, 6, 5 );
-	//point = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial( { color: 0xFF3333, shading: THREE.FlatShading } ));
-	//scene.add(point);
-	//geometry.vertices.push(new THREE.Vector3(0, 0, 0));
-	
-}
-function update() {
-	raycaster.setFromCamera(new THREE.Vector2(0,0), camera);
-	selection.visible = false;
-	collisionHelper.visible = false;
-	var intersections = raycaster.intersectObjects(scene.children);
-	var lastInter = undefined;
-	intersections.some((obj)=>{
-		if (obj.object.realPosition instanceof THREE.Vector3) {
-			lastInter = obj;
-			return true;
+world = {
+	maxDist: 16,
+	lastPos: {x: 0, z: 0},
+	chunks: [],
+	allActiveChunks: [],
+	allDeadChunks: [],
+	getBlockId: function() {
+		var candidates = [56,1,1,1,1,1,1,1,1,1,1,1,14,15,16,21,14,15,16,1,1,73];
+		let a = Math.random()*candidates.length;
+		if ((a < 1) && Math.random() < 0.99)
+			a = Math.random()*candidates.length;
+		return candidates[a|0];
+	},
+	addChunk: function(x,z) {
+		let origin = {x:x*16-8, z:z*16-8};
+		let chunk = new THREE.Group();
+		chunk.position.set(origin.x+8, 0, origin.z+8);
+		chunk.realPosition = {x:x, z:z};
+		scene.add(chunk);
+		if (!(this.chunks[z] instanceof Array))
+			this.chunks[z] = [];
+		this.chunks[z][x] = chunk;
+		this.allDeadChunks.push(chunk);
+	},
+	fillChunk: function(chunk) {
+		this.allActiveChunks.push(chunk);
+		for (var i = 0; i < 16; i++) {
+			for (var j = 0; j < 16; j++) {
+				blocks.setBlock(j-8,-3+getHeightAt(chunk.position.x+j, chunk.position.z+i)|0, i-8, this.getBlockId());
+				let block = blocks.blocks[blocks.blocks.length-1];
+				scene.remove(block);
+				chunk.add(block);
+			}
 		}
-		return false;
-	}
-	);
-	controls.update();
-	collisionHelper.position.copy(controls.player.position);
-	if (lastInter) {
-		selection.visible = true;
-		selection.position.copy(lastInter.object.realPosition);
-	}
-	if (typeof animator === "object" && animator.enabled && !animator.manualStep) {
-		animator.step();
-	} else if (typeof animator === "object" && animator.enabled) {
-		animator.updateCamera();
-	}
-	collisionHelper.visible = false;
-	renderer.render(scene, camera);
-}
-function resize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-}
-function updateMenuCookies() {
-	if (typeof setCookie === "function") {
-		setCookie("rs_statsExtended", stats.dom.children[0].enabled ? '1' : '0', 30);
-		setCookie("rs_smallMenu", (menu.iconSize === 24) ? '1' : '0');
-	}
-}
-stats.begin();
-setup();
-mainUpdateLoop();
-window.addEventListener('resize', resize, false);
-function mainUpdateLoop() {
-	stats.update();
-	menuClick = false;
-	var delta = stats.delta;
-	if (delta >= 16 * 1) {
-		if (delta < 16 * 2) {
-			stats.normalStep();
-			stats.delta -= 16;
-			update();
-		} else if (delta < 16 * 3) {
-			stats.normalStep();
-			stats.delta -= 16 * 2;
-			update();
-			update();
-		} else if (delta < 16 * 4) {
-			stats.normalStep();
-			stats.delta -= 16 * 3;
-			update();
-			update();
-			update();
-		} else if (delta < 16 * 8) {
-			stats.lagStep();
+		if (!(this.chunks[chunk.realPosition.z+1] instanceof Array) || !(this.chunks[chunk.realPosition.z+1][chunk.realPosition.x] instanceof THREE.Group))
+			this.addChunk(chunk.realPosition.x,chunk.realPosition.z+1);
+		if (!(this.chunks[chunk.realPosition.z-1] instanceof Array) || !(this.chunks[chunk.realPosition.z-1][chunk.realPosition.x] instanceof THREE.Group))
+			this.addChunk(chunk.realPosition.x,chunk.realPosition.z-1);
+		if (!(this.chunks[chunk.realPosition.z][chunk.realPosition.x+1] instanceof THREE.Group))
+			this.addChunk(chunk.realPosition.x+1,chunk.realPosition.z);
+		if (!(this.chunks[chunk.realPosition.z][chunk.realPosition.x-1] instanceof THREE.Group))
+			this.addChunk(chunk.realPosition.x-1,chunk.realPosition.z);
+	},
+	clearChunk: function(chunk) {
+		chunk.children.splice(0,16*16);
+	},
+	destroyChunk: function(chunk) {
+		scene.remove(chunk);
+		this.chunks[chunk.realPosition.z][chunk.realPosition.x]	= undefined;
+	},
+	init: function(pos) {
+		this.addChunk((pos.x/16|0),(pos.z/16|0));
+	},
+	update: function(playerPos) {
+		let pos = {x:playerPos.x, y:0, z:playerPos.z};
+		if (this.allDeadChunks.length === 0) {
+			this.init(pos);
 		} else {
-			stats.lagStep();
-			controls.releaseMouse();
+			let len, i, chunk, dist;
+			len = this.allDeadChunks.length;
+			for (i=0;i<len;i++) {
+				chunk = this.allDeadChunks[i];
+				dist = chunk.position.distanceToSquared(pos)
+				if (dist < 32*32) {
+					this.allDeadChunks.splice(i,1);
+					this.fillChunk(chunk);
+					i--;
+					len--;
+				} else if (dist > 56*56) {
+					this.allDeadChunks.splice(i,1);
+					this.destroyChunk(chunk);
+					i--;
+					len--;
+				}
+			}
+			len = this.allActiveChunks.length;
+			for (i=0;i<len;i++) {
+				chunk = this.allActiveChunks[i];
+				if (chunk.position.distanceToSquared(pos) > 36*36) {
+					this.allDeadChunks.push(this.allActiveChunks.splice(i,1)[0]);
+					this.clearChunk(chunk);
+					i--;
+					len--;
+				}
+			}
 		}
-	}
-	requestAnimationFrame(mainUpdateLoop);
+	},
+	lightUpdate: function() {}
 }
--- Script/setup.js
+++ Script/setup.js
@@ -1,36 +1,4 @@
 var menuClick = false;
 var interface = new GUI(document.body);
-document.body.onclick({preventDefault:()=>{}});
-
-//document.body.onkeydown({key:"e"});
-//interface.showHelp();
-/*var domInterface = document.getElementById("ui");
-while (domInterface.firstChild)
-	domInterface.removeChild(domInterface.firstChild);
-var domMenu = document.createElement("div")
-domMenu.className = "menu";
-var domSubmenu = document.createElement("div")
-domSubmenu.className = "submenu";
-domInterface.appendChild(domMenu);
-domInterface.appendChild(domSubmenu);
-var smallMenu = false;
-if ((typeof getCookie === "function") && (getCookie("rs_smallMenu") == '1'))
-	smallMenu = true;
-var menu = new Menu(domMenu,domSubmenu, smallMenu);
-var logger = new Logger(domInterface);
-var pointerlockSupport = ('pointerLockElement'in document || 'mozPointerLockElement'in document || 'webkitPointerLockElement'in document);
-if (!pointerlockSupport) {
-	logger.warn("Your controls will adjust accordingly.");
-	logger.warn("Your browser doesn't support pointer lock API.");
-}
-var centerMessage = new CenterMessage(domInterface, "Click to start",
-"[W, A, S, D] to move up, left, down, right\n"+
-"[Numeric Keys] to change selected block\n"+
-"[E, ESC, I] to change selected tool (top menu)\n"+
-"[Ctrl + 1, 2, 3] Also changes the selected tool\n"+
-"[Ctrl + C, Ctrl + V] copy and paste selection\n"+
-"[ +, - ] to change tools menu size"+
-"[ F ] to show debug info");
-var crosshair = new Crosshair(domInterface);
-var stats = new StatsEdited(document.body);
-stats.begin();*/
+var logger = new Logger(interface.secondary);
+options.init();
