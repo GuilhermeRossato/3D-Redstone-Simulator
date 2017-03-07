67f052b419b464dcfa339121a28242c3b81c2edb
Guilherme Rossato <guilhermrossato@gmail.com>
2017-03-06 23:43:33 -0300

    WorldHandler initialization and update starter

--- Script/setup.js
+++ Script/setup.js
@@ -2,6 +2,11 @@ var menuClick = false;
 var interface = new GUI(document.body);
 var logger = new Logger(interface.secondary);
 options.init();
+var world = new WorldHandler(interface.scene);
+
+window.requestAnimationFrame(update);
+
 // Stimuly for debug:
-//interface.showCrosshair();
+interface.showCrosshair();
 //interface.showInventory();
+
