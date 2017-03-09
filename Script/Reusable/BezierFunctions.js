fe18f2433284e35ba3a5d5c475f54298013de5b8
Guilherme Rossato <guilhermrossato@gmail.com>
2017-03-09 14:48:03 -0300

    Intuitive Interpolation Call
    Before:
    interpolate([1,6], [2, 9]).at(x)
    After:
    interpolation.add(1, 6).add(2, 9).at(x)

--- Script/Reusable/BezierFunctions.js
+++ Script/Reusable/BezierFunctions.js
@@ -1,8 +1,40 @@
 var b = (i,j,t)=>(i + (j - i) * t)
   , ib = (i,j,b)=>((i - j) == 0) ? 0 : ((i - b) / (i - j));
-  
+
+Object.defineProperty(window, "interpolation", { get: function() {
+	var pts = [];
+	return {
+		add: function(x, y) {
+			pts.push([x, y]);
+			return this;
+		},
+		_update: function() {
+		},
+		at: function(x) {
+			if (pts.length === 1)
+				return pts[0][1];
+			else if (pts.length === 2)
+				return b(pts[0][1], pts[1][1], ib(pts[0][0], pts[1][0], x));
+			else if (pts.length === 3)
+				return b(b(pts[0][1], pts[1][1], ib(pts[0][0], pts[1][0], x)), b(pts[1][1], pts[2][1], ib(pts[1][0], pts[2][0], x)), ib(pts[0][0], pts[2][0], x));
+			else if (pts.length === 4)
+				return b(b(b(pts[0][1], pts[1][1], ib(pts[0][0], pts[1][0], x)), b(pts[1][1], pts[2][1], ib(pts[1][0], pts[2][0], x)), ib(pts[0][0], pts[2][0], x)), b(b(pts[1][1], pts[2][1], ib(pts[1][0], pts[2][0], x)), b(pts[2][1], pts[3][1], ib(pts[2][0], pts[3][0], x)), ib(pts[1][0], pts[3][0], x)), ib(pts[0][0], pts[3][0], x));
+			else {
+				this.warn();
+				return 0;
+			}
+		},
+		warn: function(len) {
+			console.warn("Consider creating a more performant interpolation method. This was not made to efficiently interpolate "+ len + " points");
+			this.warn = () => undefined;
+		}
+	}
+}});
+
 function interpolate(pt0, pt1, pt2, pt3) {
 	ret = {};
+	if (typeof pt0 === "number")
+		console.warn("EXPECTED ARRAY!!");
 	if (typeof pt2 === "undefined")
 		ret.at = x=>b(pt0[1], pt1[1], ib(pt0[0], pt1[0], x));
 	else if (typeof pt3 === "undefined")
