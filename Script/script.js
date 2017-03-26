5b80f6df6e7d999f2028dcd789f9923ef7359322
Guilherme Rossato <guilhermrossato@gmail.com>
2017-03-26 18:24:00 -0300

    Setup Cleanup
    Removed unecessary functions and calls

--- Script/script.js
+++ Script/script.js
@@ -1,19 +1,3 @@
-function getHeightAt(x, z) {
-	return (perlin(x / 30, z / 29) + 1) * 4.5;
-}
-
-function generateArea(pos, side) {
-	let i, j;
-	var map = new Uint8Array(side * side);
-	for (i = 0; i < side; i++) {
-		for (j = 0; j < side; j++) {
-			let height = getHeightAt(i+pos.z, j+pos.x);
-			map[i + j * side] = height;
-		}
-	}
-	return map;
-}
-
 function placePlayer(x,y,z) {
 	x = x || -32;
 	y = y || 11;
@@ -23,5 +7,3 @@ function placePlayer(x,y,z) {
 	player.controls.player.rotation.y = -0.772;
 	player.controls.player.children[0].rotation.x = -0.523
 }
-
-world.generate();
--- Script/setup.js
+++ Script/setup.js
@@ -1,12 +1,8 @@
-var menuClick = false;
+var input = new InputListener();
+var persister = new SettingsPersister();
 var interface = new GUI(document.body);
 var logger = new Logger(interface.secondary);
-options.init();
 var world = new WorldHandler(interface.scene);
-
-//window.requestAnimationFrame(simpleUpdate);
-
 // Stimuly for debug:
 interface.showCrosshair();
 //interface.showInventory();
-
