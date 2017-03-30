5cdafe6b5610e021a44a220f7965ad06bae80729
Guilherme Rossato <guilhermrossato@gmail.com>
2017-03-30 15:03:30 -0300

    Chunk Class Base

--- Script/Classes/Chunk/Chunk.js
+++ Script/Classes/Chunk/Chunk.js
@@ -1,24 +1,53 @@
 function Chunk(x, y, z, scene, material) {
-	this.centerMesh = function() {
-		this.mesh.geometry.boundingSphere.center.set(x, y, z);
+	this.meshRecreation = true;
+	this.disableMeshRecreation = function() {
+		this.meshRecreation = false;
 	}
+	this.enableMeshRecreation = function() {
+		this.meshRecreation = true;
+	}
+	this.material = material;
 	if (!material)
 		throw "rock";
 	else
-		this.createMesh(scene, material);
+		this.createMesh(scene);
 }
+
+var counting = 0;
 
 Chunk.prototype = {
 	constructor: Chunk,
-	createMesh: function(scene, material) {
+	createMesh: function(scene) {
 		this.geometry = new THREE.Geometry();
-		this.mesh = new THREE.Mesh(this.geometry, material);
-		//this.centerMesh();
+		this.mesh = new THREE.Mesh(this.geometry, this.material);
 		scene.add(this.mesh);
 	},
+	recreateMesh: function() {
+		this.dirty = false;
+		let geo = new THREE.Geometry();
+		geo.vertices = this.geometry.vertices;
+		geo.faces = this.geometry.faces;
+		geo.faceVertexUvs = this.geometry.faceVertexUvs;
+		this.geometry.dispose();
+		this.geometry.vertices = undefined;
+		this.geometry.faces = undefined;
+		this.geometry.boundingSphere = undefined;
+		this.geometry = geo;
+		this.geometry.boundingSphere = null;
+		this.mesh.geometry = this.geometry;
+	},
 	addMesh: function(mesh) {
+		counting ++;
+		//console.log(mesh.position, counting);
 		mesh.updateMatrix();
 		this.geometry.merge(mesh.geometry, mesh.matrix);
-		this.mesh.updateMatrix();
+		if (this.geometry.boundingSphere) {
+			if (this.meshRecreation)
+				this.recreateMesh();
+			else
+				this.dirty = true;
+		}
+		//this.mesh.updateMatrix();
+		//return 
 	}
 }
