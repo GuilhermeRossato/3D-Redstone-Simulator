d623fc10ece2b4a87b7eb0f2038fc41af6644d20
Guilherme Rossato <guilhermrossato@gmail.com>
2017-07-13 22:53:07 -0300

    Support to run in other drives
    So that you can have the project in a different drive than the browser you're running the project with.

--- Tools/run.bat
+++ Tools/run.bat
@@ -1,5 +1,12 @@
 @echo off
+set browserPath=C:\Program Files (x86)\Google\Chrome\Application
+set browserExec=chrome.exe
+set toolsPath=%~dp0
+set projectPath=%toolsPath%..
+set "browserParams=--disable-web-security --user-data-dir=^"%localappdata%\Google\Chrome\User Data^" ^"%projectPath%\index.html^" --profile-directory=^"Default^""
 cd ..
-set OLDDIR=%CD%
-cd "C:\Program Files (x86)\Google\Chrome\Application"
-chrome.exe --disable-web-security --user-data-dir="%appdata%\..\Local\Google\Chrome\User Data" "%OLDDIR%\index.html" --profile-directory="Default"
+if not (%projectPath:~0,1% == %browserPath:~0,1%) %browserPath:~0,2%
+cd "%browserPath%"
+%browserExec% %browserParams%
+if not (%projectPath:~0,1% == %browserPath:~0,1%) %projectPath:~0,2%
+cd %toolsPath%
