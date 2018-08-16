6782cd20709576d0ff686219db8ea1bceff4e136
Guilherme Rossato <guilhermrossato@gmail.com>
2018-08-16 17:06:15 -0300

    Added watchdog script on loading screen
    To warn when it fails to load

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,24 @@ define(["scripts/classes/Application.js"], (Application)=>{
 	this.app = new Application();
 });
 
-var obj;
-if (obj = document.querySelector(".footer > .no-script")) {
-	obj.style.display = "none";
-}
+(function clear_javascript_required_text() {
+	var obj;
+	if (obj = document.querySelector(".footer > .no-script")) {
+		obj.style.display = "none";
+	}
+})();
+
+setTimeout(function loading_watchdog() {
+	var loading_span = document.querySelector(".content > .description > .text");
+	if (loading_span.innerText === "Downloading javascript files") {
+		loading_span.innerHTML = "Something went wrong while loading the app.<br>Please contact support.";
+		loading_span.style.textAlign = "center";
+		loading_span.style.width = "100%";
+		try {
+			document.querySelector(".content > .description > .percentage").style.display = "none";
+		} catch (e) {}
+		try {
+			document.querySelector(".content > .bar").style.display = "none";
+		} catch (e) {}
+	}
+}, 1000);
