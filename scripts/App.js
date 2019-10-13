cc61726bd45033166333e124a3af3b4877c7c102
Guilherme Rossato <guilhermrossato@gmail.com>
2019-10-13 12:50:27 -0300

    Experimenting with InstancedBuffer

--- scripts/App.js
+++ scripts/App.js
@@ -40,7 +40,7 @@ export default class App {
 	update() {
 		(this.screen) && (this.screen.update) && (this.screen.update());
 		// Update camera around the object
-		if (this.frame < 9000) {
+		if (this.frame < 500) {
 			this.frame++;
 		} else {
 			this.frame = 0;
@@ -52,12 +52,12 @@ export default class App {
 			}
 		}
 		if (this.graphics.camera) {
-			var angle = 2*Math.PI*(this.frame/9000)
+			var angle = 2*Math.PI*(this.frame/1100)
 			//angle = Math.PI*1.7;
 			var scale = window.scale || 0.25;
 			this.graphics.camera.position.x = 0.41+Math.cos(angle)*10*scale;
 			this.graphics.camera.position.z = Math.sin(angle)*10*scale;
-			this.graphics.camera.position.y = 0+68*scale;
+			this.graphics.camera.position.y = 0+10*scale;
 			this.graphics.camera.lookAt(0, 0, 0);
 		}
 	}
@@ -73,60 +73,77 @@ export default class App {
 	}
 	mockWorld() {
 		const size = 8;
-		this.world.set(0, 3, 0, 1);
+		this.world.set(0, -2, 0, 1);
 		for (var i = 0; i < size; i++) {
 			for (var j = 0; j < size; j++) {
-				this.world.set(i-size/2, (i%3==0||j%3==0)?0:2+j%3-i%3, j-size/2, (i%3==0||j%3==0)?2:3);
+				//this.world.set(i-size/2, (i%3==0||j%3==0)?0:2+j%3-i%3, j-size/2, (i%3==0||j%3==0)?2:3);
 			}
 		}
 		this.addTests();
 	}
 	async addTests() {
 		const scene = this.world.scene;
-		const material = TextureService.getMaterial();
+		//const material = TextureService.getMaterial();
 		window.scene = scene;
-		window.material = material;
+		//window.material = material;
 		window.THREE = THREE;
 		
-		var geometry = new THREE.BoxBufferGeometry( 1, 1, 1 );
-		
-		var material2 = new THREE.MeshLambertMaterial({
-			map: TextureService.texture,
-			color: 0x555555,
-			wireframe: false,
-			transparent: true,
-			alphaMap: TextureService.alphaMap
+
+		var geometry = new THREE.PlaneBufferGeometry(2, 2);
+
+		const instanced = new THREE.InstancedBufferGeometry();
+		instanced.attributes.position = geometry.attributes.position;
+		instanced.attributes.uv = geometry.attributes.uv;
+		instanced.index = geometry.index;
+
+		const positionAttribute = new THREE.InstancedBufferAttribute( new Float32Array([-1.1, 1.1, 0, 1.1, 1.1, 0, -1.1, -1.1, 0, 1.1, -1.1, 0]), 3);
+		instanced.addAttribute("instancePosition", positionAttribute);
+		const uvAttribute = new THREE.InstancedBufferAttribute( new Float32Array([0, 1, 1, 1, 0, 0, 1, 0]), 2);
+		instanced.addAttribute("instanceUv", uvAttribute);
+
+		const material2 = new THREE.ShaderMaterial({
+			uniforms: {
+				texture1: { value: TextureService.texture },
+				textureDivision: { value: new THREE.Vector2(8,8) },
+				time: {value: 0}
+			},
+			vertexShader: `
+				precision highp float;
+
+				uniform vec2 textureDivision;
+				uniform float time;
+
+				attribute vec3 instancePosition;
+				attribute vec2 instanceUv;
+
+				varying vec2 vUv;
+
+				void main(){
+				vec2 slices = vec2(1.0) / textureDivision;
+					vUv = slices * instanceUv + slices * uv;
+				vec3 pos = position + instancePosition;
+				pos += normalize(instancePosition) * (sin(time) * 0.5 + 0.5);
+
+					gl_Position = projectionMatrix * modelViewMatrix * vec4( pos, 1.0 );
+				}
+			`,
+			fragmentShader: `
+				precision highp float;
+
+				uniform sampler2D texture1;
+
+				varying vec2 vUv;
+
+				void main() {
+					gl_FragColor = texture2D(texture1, vUv);
+				}
+			`
 		});
-		material2.side = THREE.FrontSide;
 
-		var count = 100;
-
-		var mesh = new THREE.InstancedMesh( geometry, material2, count );
-
-		var dummy = new THREE.Object3D();
-
-		for ( var i = 0; i < count; i ++ ) {
-
-			dummy.position.set(
-				Math.random() * 20 - 10,
-				Math.random() * 20 - 10,
-				Math.random() * 20 - 10
-			);
-
-			dummy.rotation.set(
-				Math.random() * Math.PI,
-				Math.random() * Math.PI,
-				Math.random() * Math.PI
-			);
-
-			dummy.updateMatrix();
-
-			mesh.setMatrixAt( i, dummy.matrix );
-
-		}
-
-		mesh.position.set(0, 4, 0);
-		scene.add( mesh );
+		const mesh = new THREE.Mesh(instanced, material2);
+		mesh.position.set(0, 0, 0);
+		scene.add(mesh);
+		scene.add(new THREE.AxesHelper(0.2));
 	}
 	async loadWorld() {
 		await this.loader.loadWorld();
