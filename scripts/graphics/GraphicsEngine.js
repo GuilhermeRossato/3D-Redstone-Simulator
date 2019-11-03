ac7a4f074940ea1944cad978ee1e5239641a52a5
Guilherme Rossato <guilhermrossato@gmail.com>
2019-11-03 13:49:14 -0300

    Simplified GraphicsEngine's aoMultiplier to a float

--- scripts/graphics/GraphicsEngine.js
+++ scripts/graphics/GraphicsEngine.js
@@ -61,11 +61,14 @@ export default class GraphicsEngine {
 		this.camera.position.z = 1;
 		const scene = this.scene = new THREE.Scene();
 
-		this.constructor.addLightToScene(scene);
+		// this.constructor.addLightToScene(scene);
 
 		let context = this.canvas.getContext('webgl2');
 		if (!context) {
 			context = this.canvas.getContext('webgl');
+		}
+		if (!context) {
+			throw new Error("WebGL not supported");
 		}
 		const rendererConfig = {
 			canvas: this.canvas,
@@ -193,145 +196,157 @@ export default class GraphicsEngine {
 						discard;
 					}
 
-					vec3 aoMult;
+					float aoMult;
 
 					if (vLightness.y == 0.0) { // nothing
-						aoMult = vec3(1.0);
+						aoMult = (1.0);
 					} else if (vLightness.y == 208.0) {
 						// all corners but bottom right
-						aoMult = vec3(1.0 - max(min(vRelativeUv.y, vRelativeUv.x),max(min(vRelativeUv.y, 1.0 - vRelativeUv.x),min(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x))) * AO_IMPACT);
+						aoMult = (1.0 - max(min(vRelativeUv.y, vRelativeUv.x),max(min(vRelativeUv.y, 1.0 - vRelativeUv.x),min(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x))) * AO_IMPACT);
 					} else if (vLightness.y == 176.0) {
 						// all corners but bottom left
-						aoMult = vec3(1.0 - max(min(vRelativeUv.y, vRelativeUv.x),max(min(vRelativeUv.y, 1.0 - vRelativeUv.x),min(1.0 - vRelativeUv.y, vRelativeUv.x))) * AO_IMPACT);
+						aoMult = (1.0 - max(min(vRelativeUv.y, vRelativeUv.x),max(min(vRelativeUv.y, 1.0 - vRelativeUv.x),min(1.0 - vRelativeUv.y, vRelativeUv.x))) * AO_IMPACT);
 					} else if (vLightness.y == 112.0) {
 						// all corners but top left
-						aoMult = vec3(1.0 - max(min(vRelativeUv.y, vRelativeUv.x),max(min(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x),min(1.0 - vRelativeUv.y, vRelativeUv.x))) * AO_IMPACT);
+						aoMult = (1.0 - max(min(vRelativeUv.y, vRelativeUv.x),max(min(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x),min(1.0 - vRelativeUv.y, vRelativeUv.x))) * AO_IMPACT);
 					} else if (vLightness.y == 224.0) {
 						// all corners but top right
-						aoMult = vec3(1.0 - max(min(vRelativeUv.y, 1.0 - vRelativeUv.x),max(min(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x),min(1.0 - vRelativeUv.y, vRelativeUv.x))) * AO_IMPACT);
+						aoMult = (1.0 - max(min(vRelativeUv.y, 1.0 - vRelativeUv.x),max(min(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x),min(1.0 - vRelativeUv.y, vRelativeUv.x))) * AO_IMPACT);
 					} else if (vLightness.y == 128.0) {
 						 // top left
-						aoMult = vec3(1.0 - (1.0 - max(1.0 - vRelativeUv.y, vRelativeUv.x)) * AO_IMPACT);
+						aoMult = (1.0 - (1.0 - max(1.0 - vRelativeUv.y, vRelativeUv.x)) * AO_IMPACT);
 					} else if (vLightness.y == 1.0 || vLightness.y == 17.0 || vLightness.y == 129.0 || vLightness.y == 145.0) {
 						// top
-						aoMult = vec3(1.0 - vRelativeUv.y * AO_IMPACT);
+						aoMult = (1.0 - vRelativeUv.y * AO_IMPACT);
 					} else if (vLightness.y == 16.0) {
 						// top right
-						aoMult = vec3(1.0 - (1.0 - max(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x)) * AO_IMPACT);
+						aoMult = (1.0 - (1.0 - max(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x)) * AO_IMPACT);
 					} else if (vLightness.y == 8.0 || vLightness.y == 72.0 || vLightness.y == 136.0 || vLightness.y == 200.0) {
 						// left
-						aoMult = vec3(1.0 - (1.0 - vRelativeUv.x) * AO_IMPACT);
+						aoMult = (1.0 - (1.0 - vRelativeUv.x) * AO_IMPACT);
 					} else if (vLightness.y == 2.0 || vLightness.y == 18.0 || vLightness.y == 34.0 || vLightness.y == 50.0) {
 						// right
-						aoMult = vec3(1.0 - vRelativeUv.x * AO_IMPACT);
+						aoMult = (1.0 - vRelativeUv.x * AO_IMPACT);
 					} else if (vLightness.y == 64.0) {
 						// bottom left
-						aoMult = vec3(1.0 - (1.0 - max(vRelativeUv.y, vRelativeUv.x)) * AO_IMPACT);
+						aoMult = (1.0 - (1.0 - max(vRelativeUv.y, vRelativeUv.x)) * AO_IMPACT);
 					} else if (vLightness.y == 4.0 || vLightness.y == 36.0 || vLightness.y == 68.0 || vLightness.y == 100.0) {
 						// bottom
-						aoMult = vec3(1.0 - (1.0 - vRelativeUv.y) * AO_IMPACT);
+						aoMult = (1.0 - (1.0 - vRelativeUv.y) * AO_IMPACT);
 					} else if (vLightness.y == 32.0) {
 						// bottom right
-						aoMult = vec3(1.0 - (1.0 - max(vRelativeUv.y, 1.0 - vRelativeUv.x)) * AO_IMPACT);
+						aoMult = (1.0 - (1.0 - max(vRelativeUv.y, 1.0 - vRelativeUv.x)) * AO_IMPACT);
 					} else if (vLightness.y == 9.0 || vLightness.y == 25.0 || vLightness.y == 73.0 || vLightness.y == 89.0 || vLightness.y == 137.0 || vLightness.y == 153.0 || vLightness.y == 201.0 || vLightness.y == 217.0) {
 						// top, left
-						aoMult = vec3(1.0 - (1.0 - min(1.0 - vRelativeUv.y, vRelativeUv.x)) * AO_IMPACT);
+						aoMult = (1.0 - (1.0 - min(1.0 - vRelativeUv.y, vRelativeUv.x)) * AO_IMPACT);
 					} else if (vLightness.y == 3.0 || vLightness.y == 19.0 || vLightness.y == 35.0 || vLightness.y == 51.0 || vLightness.y == 131.0 || vLightness.y == 147.0 || vLightness.y == 163.0 || vLightness.y == 179.0) {
 						// top, right
-						aoMult = vec3(1.0 - (1.0 - min(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x)) * AO_IMPACT);
+						aoMult = (1.0 - (1.0 - min(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x)) * AO_IMPACT);
 					} else if (vLightness.y == 6.0 || vLightness.y == 22.0 || vLightness.y == 38.0 || vLightness.y == 54.0 || vLightness.y == 70.0 || vLightness.y == 86.0 || vLightness.y == 102.0 || vLightness.y == 118.0) {
 						// bottom, right
-						aoMult = vec3(1.0 - (1.0 - min(vRelativeUv.y, 1.0 - vRelativeUv.x)) * AO_IMPACT);
+						aoMult = (1.0 - (1.0 - min(vRelativeUv.y, 1.0 - vRelativeUv.x)) * AO_IMPACT);
 					} else if (vLightness.y == 12.0 || vLightness.y == 44.0 || vLightness.y == 76.0 || vLightness.y == 108.0 || vLightness.y == 140.0 || vLightness.y == 172.0 || vLightness.y == 204.0 || vLightness.y == 236.0) {
 						// bottom, left
-						aoMult = vec3(1.0 - (1.0 - min(vRelativeUv.y, vRelativeUv.x)) * AO_IMPACT);
+						aoMult = (1.0 - (1.0 - min(vRelativeUv.y, vRelativeUv.x)) * AO_IMPACT);
 					} else if (mod(vLightness.y, 2.0) == 1.0 && mod(floor(vLightness.y/2.0), 2.0) == 1.0 && mod(floor(vLightness.y/4.0), 2.0) == 1.0 && mod(floor(vLightness.y/8.0), 2.0) == 0.0) {
 						// top, right, bottom, no left
-						aoMult = vec3(1.0 - (1.0 - min(0.5-abs(vRelativeUv.y-0.5), 1.0-vRelativeUv.x))*AO_IMPACT);
+						aoMult = (1.0 - (1.0 - min(0.5-abs(vRelativeUv.y-0.5), 1.0-vRelativeUv.x))*AO_IMPACT);
 					} else if (mod(vLightness.y, 2.0) == 0.0 && mod(floor(vLightness.y/2.0), 2.0) == 1.0 && mod(floor(vLightness.y/4.0), 2.0) == 1.0 && mod(floor(vLightness.y/8.0), 2.0) == 1.0) {
 						// right, bottom, left, no right
-						aoMult = vec3(1.0 - (1.0 - min(0.5-abs(vRelativeUv.x-0.5), vRelativeUv.y))*AO_IMPACT);
+						aoMult = (1.0 - (1.0 - min(0.5-abs(vRelativeUv.x-0.5), vRelativeUv.y))*AO_IMPACT);
 					} else if (mod(vLightness.y, 2.0) == 1.0 && mod(floor(vLightness.y/2.0), 2.0) == 0.0 && mod(floor(vLightness.y/4.0), 2.0) == 1.0 && mod(floor(vLightness.y/8.0), 2.0) == 1.0) {
 						// top, bottom, left, no right
-						aoMult = vec3(1.0 - (1.0 - min(0.5-abs(vRelativeUv.y-0.5), vRelativeUv.x))*AO_IMPACT);
+						aoMult = (1.0 - (1.0 - min(0.5-abs(vRelativeUv.y-0.5), vRelativeUv.x))*AO_IMPACT);
 					} else if (mod(vLightness.y, 2.0) == 1.0 && mod(floor(vLightness.y/2.0), 2.0) == 1.0 && mod(floor(vLightness.y/4.0), 2.0) == 0.0 && mod(floor(vLightness.y/8.0), 2.0) == 1.0) {
 						// top, right, left, no bottom
-						aoMult = vec3(1.0 - (1.0 - min(0.5-abs(vRelativeUv.x-0.5), 1.0-vRelativeUv.y))*AO_IMPACT);
+						aoMult = (1.0 - (1.0 - min(0.5-abs(vRelativeUv.x-0.5), 1.0-vRelativeUv.y))*AO_IMPACT);
 					} else if (vLightness.y == 144.0) {
 						// top left, top right
-						aoMult = vec3(1.0 - max(
+						aoMult = (1.0 - max(
 							min(vRelativeUv.y, 1.0 - vRelativeUv.x),
 							min(vRelativeUv.y, vRelativeUv.x)
 						) * AO_IMPACT);
 					} else if (vLightness.y == 48.0) {
 						// top right, bottom right
-						aoMult = vec3(1.0 - max(
+						aoMult = (1.0 - max(
 							min(1.0 - vRelativeUv.y, vRelativeUv.x),
 							min(vRelativeUv.y, vRelativeUv.x)
 						) * AO_IMPACT);
 					} else if (vLightness.y == 96.0) {
 						// bottom left, bottom right
-						aoMult = vec3(1.0 - max(
+						aoMult = (1.0 - max(
 							min(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x),
 							min(1.0 - vRelativeUv.y, vRelativeUv.x)
 						) * AO_IMPACT);
 					} else if (vLightness.y == 192.0) {
 						// top left, bottom left
-						aoMult = vec3(1.0 - max(
+						aoMult = (1.0 - max(
 							min(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x),
+							min(vRelativeUv.y, 1.0 - vRelativeUv.x)
+						) * AO_IMPACT);
+					} else if (vLightness.y == 80.0) {
+						 // top right, bottom left
+						aoMult = (1.0 - max(
+							min(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x),
+							min(vRelativeUv.y, vRelativeUv.x)
+						) * AO_IMPACT);
+					} else if (vLightness.y == 160.0) {
+						 // top left, bottom right
+						aoMult = (1.0 - max(
+							min(1.0 - vRelativeUv.y, vRelativeUv.x),
 							min(vRelativeUv.y, 1.0 - vRelativeUv.x)
 						) * AO_IMPACT);
 					} else if (vLightness.y == 65.0 || vLightness.y == 81.0 || vLightness.y == 193.0 || vLightness.y == 209.0) {
 						// top, bottom left
-						aoMult = vec3(1.0 - max(
+						aoMult = (1.0 - max(
 							min(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x),
 							vRelativeUv.y
 						) * AO_IMPACT);
 					} else if (vLightness.y == 33.0 || vLightness.y == 49.0 || vLightness.y == 161.0 || vLightness.y == 177.0) {
 						// top, bottom right
-						aoMult = vec3(1.0 - max(
+						aoMult = (1.0 - max(
 							min(1.0 - vRelativeUv.y, vRelativeUv.x),
 							vRelativeUv.y
 						) * AO_IMPACT);
 					} else if (vLightness.y == 130.0 || vLightness.y == 146.0 || vLightness.y == 162.0 || vLightness.y == 178.0) {
 						// right, top left
-						aoMult = vec3(1.0 - max(
+						aoMult = (1.0 - max(
 							min(vRelativeUv.y, 1.0 - vRelativeUv.x),
 							vRelativeUv.x
 						) * AO_IMPACT);
 					} else if (vLightness.y == 66.0 || vLightness.y == 82.0 || vLightness.y == 98.0 || vLightness.y == 114.0) {
 						// right, bottom left
-						aoMult = vec3(1.0 - max(
+						aoMult = (1.0 - max(
 							min(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x),
 							vRelativeUv.x
 						) * AO_IMPACT);
 					} else if (vLightness.y == 132.0 || vLightness.y == 164.0 || vLightness.y == 196.0 || vLightness.y == 228.0) {
 						// bottom, top left
-						aoMult = vec3(1.0 - max(
+						aoMult = (1.0 - max(
 							min(1.0 - vRelativeUv.x, vRelativeUv.y),
 							1.0 - vRelativeUv.y
 						) * AO_IMPACT);
 					} else if (vLightness.y == 20.0 || vLightness.y == 52.0 || vLightness.y == 84.0 || vLightness.y == 116.0) {
 						// bottom, top right
-						aoMult = vec3(1.0 - max(
+						aoMult = (1.0 - max(
 							min(vRelativeUv.x, vRelativeUv.y),
 							1.0 - vRelativeUv.y
 						) * AO_IMPACT);
 					} else if (vLightness.y == 24.0 || vLightness.y == 88.0 || vLightness.y == 152.0 || vLightness.y == 216.0) {
 						// left, top right
-						aoMult = vec3(1.0 - max(
+						aoMult = (1.0 - max(
 							min(vRelativeUv.x, vRelativeUv.y),
 							1.0 - vRelativeUv.x
 						) * AO_IMPACT);
 					} else if (vLightness.y == 40.0 || vLightness.y == 104.0 || vLightness.y == 168.0 || vLightness.y == 232.0) {
 						// left, bottom right
-						aoMult = vec3(1.0 - max(
+						aoMult = (1.0 - max(
 							min(vRelativeUv.x, 1.0 - vRelativeUv.y),
 							1.0 - vRelativeUv.x
 						) * AO_IMPACT);
 					} else if (vLightness.y == 97.0 || vLightness.y == 113.0 || vLightness.y == 225.0 || vLightness.y == 241.0) {
 						// top, bottom left, bottom right
-						aoMult = vec3(1.0 - max(
+						aoMult = (1.0 - max(
 							max(
 								min(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x),
 								min(1.0 - vRelativeUv.y, vRelativeUv.x)
@@ -340,7 +355,7 @@ export default class GraphicsEngine {
 						) * AO_IMPACT);
 					} else if (vLightness.y == 194.0 || vLightness.y == 210.0 || vLightness.y == 226.0 || vLightness.y == 242.0) {
 						// top, bottom left, bottom right
-						aoMult = vec3(1.0 - max(
+						aoMult = (1.0 - max(
 							max(
 								min(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x),
 								min(vRelativeUv.y, 1.0 - vRelativeUv.x)
@@ -349,7 +364,7 @@ export default class GraphicsEngine {
 						) * AO_IMPACT);
 					} else if (vLightness.y == 148.0 || vLightness.y == 180.0 || vLightness.y == 212.0 || vLightness.y == 244.0) {
 						// bottom, top left, top right
-						aoMult = vec3(1.0 - max(
+						aoMult = (1.0 - max(
 							max(
 								min(vRelativeUv.y, 1.0 - vRelativeUv.x),
 								min(vRelativeUv.y, vRelativeUv.x)
@@ -358,7 +373,7 @@ export default class GraphicsEngine {
 						) * AO_IMPACT);
 					} else if (vLightness.y == 56.0 || vLightness.y == 120.0 || vLightness.y == 184.0 || vLightness.y == 248.0) {
 						// bottom, top left, top right
-						aoMult = vec3(1.0 - max(
+						aoMult = (1.0 - max(
 							max(
 								min(1.0 - vRelativeUv.y, vRelativeUv.x),
 								min(vRelativeUv.y, vRelativeUv.x)
@@ -367,16 +382,16 @@ export default class GraphicsEngine {
 						) * AO_IMPACT);
 					} else if (mod(vLightness.y, 2.0) == 1.0 && mod(floor(vLightness.y/4.0), 2.0) == 1.0 && mod(floor(vLightness.y/2.0), 2.0) == 1.0 && mod(floor(vLightness.y/8.0), 2.0) == 1.0) {
 						// all 4 sides						
-						aoMult = vec3(1.0 - max(max(max(vRelativeUv.y, 1.0-vRelativeUv.x), vRelativeUv.x), 1.0 - vRelativeUv.y) * AO_IMPACT);
+						aoMult = (1.0 - max(max(max(vRelativeUv.y, 1.0-vRelativeUv.x), vRelativeUv.x), 1.0 - vRelativeUv.y) * AO_IMPACT);
 					} else if ( mod(vLightness.y, 2.0) == 1.0 && mod(floor(vLightness.y/4.0), 2.0) == 1.0 ) {
 						// top, bottom
-						aoMult = vec3(1.0 - max(vRelativeUv.y, 1.0 - vRelativeUv.y) * AO_IMPACT);
+						aoMult = (1.0 - max(vRelativeUv.y, 1.0 - vRelativeUv.y) * AO_IMPACT);
 					} else if ( mod(floor(vLightness.y/2.0), 2.0) == 1.0 && mod(floor(vLightness.y/8.0), 2.0) == 1.0 ) {
 						// left, right
-						aoMult = vec3(1.0 - max(vRelativeUv.x, 1.0 - vRelativeUv.x) * AO_IMPACT);
+						aoMult = (1.0 - max(vRelativeUv.x, 1.0 - vRelativeUv.x) * AO_IMPACT);
 					} else if (vLightness.y == 240.0) {
 						// all corners
-						aoMult = vec3(1.0 - max(max(
+						aoMult = (1.0 - max(max(
 							min(vRelativeUv.y, vRelativeUv.x),
 							min(1.0 - vRelativeUv.y, vRelativeUv.x)
 						), max(
@@ -385,16 +400,16 @@ export default class GraphicsEngine {
 						)) * AO_IMPACT);
 					} else if (mod(vLightness.y, 2.0) == 1.0 && mod(floor(vLightness.y/2.0), 2.0) == 0.0 && mod(floor(vLightness.y/4.0), 2.0) == 0.0 && mod(floor(vLightness.y/8.0), 2.0) == 1.0) {
 						// top, left, probably bottom right
-						aoMult = vec3(1.0 - (1.0 - min(max(vRelativeUv.y, 1.0 - vRelativeUv.x), min(1.0 - vRelativeUv.y, vRelativeUv.x))) * AO_IMPACT);
+						aoMult = (1.0 - (1.0 - min(max(vRelativeUv.y, 1.0 - vRelativeUv.x), min(1.0 - vRelativeUv.y, vRelativeUv.x))) * AO_IMPACT);
 					} else if (mod(vLightness.y, 2.0) == 1.0 && mod(floor(vLightness.y/2.0), 2.0) == 1.0 && mod(floor(vLightness.y/4.0), 2.0) == 0.0 && mod(floor(vLightness.y/8.0), 2.0) == 0.0) {
 						// top, right, probably bottom left
-						aoMult = vec3(1.0 - (1.0 - min(max(vRelativeUv.y, vRelativeUv.x), min(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x))) * AO_IMPACT);
+						aoMult = (1.0 - (1.0 - min(max(vRelativeUv.y, vRelativeUv.x), min(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x))) * AO_IMPACT);
 					} else if (mod(vLightness.y, 2.0) == 0.0 && mod(floor(vLightness.y/2.0), 2.0) == 1.0 && mod(floor(vLightness.y/4.0), 2.0) == 1.0 && mod(floor(vLightness.y/8.0), 2.0) == 0.0) {
 						// bottom, right, probably top left
-						aoMult = vec3(1.0 - (1.0 - min(max(1.0 - vRelativeUv.y, vRelativeUv.x), min(vRelativeUv.y, 1.0 - vRelativeUv.x))) * AO_IMPACT);
+						aoMult = (1.0 - (1.0 - min(max(1.0 - vRelativeUv.y, vRelativeUv.x), min(vRelativeUv.y, 1.0 - vRelativeUv.x))) * AO_IMPACT);
 					} else if (mod(vLightness.y, 2.0) == 0.0 && mod(floor(vLightness.y/2.0), 2.0) == 0.0 && mod(floor(vLightness.y/4.0), 2.0) == 1.0 && mod(floor(vLightness.y/8.0), 2.0) == 1.0) {
 						// left, bottom, probably top right
-						aoMult = vec3(1.0 - (1.0 - min(max(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x), min(vRelativeUv.y, vRelativeUv.x))) * AO_IMPACT);
+						aoMult = (1.0 - (1.0 - min(max(1.0 - vRelativeUv.y, 1.0 - vRelativeUv.x), min(vRelativeUv.y, vRelativeUv.x))) * AO_IMPACT);
 					}
 
 					gl_FragColor = vec4(color.xyz * vLightness.x * aoMult, 1.0);
