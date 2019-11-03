86d5f8659c71f69d82ad4d2a667216647771468b
Guilherme Rossato <guilhermrossato@gmail.com>
2019-11-03 13:49:24 -0300

    Made BlockData more generic

--- scripts/data/BlockData.js
+++ scripts/data/BlockData.js
@@ -23,6 +23,13 @@ export default [
 	]
 }, {
 	name: "Stone Brick",
-	texture: {x:6, y:0}
+	texture: [
+		{x:6, y:0},
+		{x:6, y:0},
+		{x:6, y:0},
+		{x:6, y:0},
+		{x:6, y:0},
+		{x:6, y:0}
+	]
 }
 ];
