cbf0b697f54404e6ed06d541d4fd8d85705bfd7c
Guilherme Rossato <guilhermrossato@gmail.com>
2024-03-18 22:24:27 -0300

    Removed old unused tests

--- test/WorldHandlerTest.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-
-import * as WorldHandler from '../frontend/src/modules/WorldHandler.js';
-
-if (!WorldHandler.get) {
-    throw new Error('World handler is missing the get function');
-}
