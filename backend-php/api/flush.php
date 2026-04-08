47b3f7c2a2506c2b65d256b6c7bb667b37b25dbf
Guilherme Rossato <guilhermrossato@gmail.com>
2026-04-08 13:30:41 -0300

    Fixed flush return functionality

--- backend-php/api/flush.php
+++ backend-php/api/flush.php
@@ -12,7 +12,15 @@ $methods = get_class_methods($memcache);
 
 echo "Available Memcache methods: " . implode(", ", $methods);
 
-if (isset($_GET['ret'])) {
+if (isset($_GET['ret'])&&!empty($_GET['ret'])) {
     $ret = $_GET['ret'];
-    header("Location: $ret");
+    echo "<script>" . PHP_EOL;
+    echo "window.location.href = " . json_encode($ret) . ";" . PHP_EOL;
+    echo "</script>" . PHP_EOL;
+
+    echo "<script>" . PHP_EOL;
+    echo "setTimeout(function() {" . PHP_EOL;
+    echo "  window.location.href = " . json_encode($ret) . ";" . PHP_EOL;
+    echo "}, 1000);" . PHP_EOL;
+    echo "</script>" . PHP_EOL;
 }
