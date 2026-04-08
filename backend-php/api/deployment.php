ef84a55cc176944ade03055ac1f9613da8c5529c
Guilherme Rossato <guilhermrossato@gmail.com>
2026-04-08 13:15:41 -0300

    Added updated deployment script

--- backend-php/api/deployment.php
+++ backend-php/api/deployment.php
@@ -1,75 +1,63 @@
 <?php
 
 $appVersionId = getenv('HTTP_X_APPENGINE_APPVERSIONID');
-$i = strpos($appVersionId, '20');
-if ($i === false || $i === 0) {
-  error_log("Unexpected app version ID format: " . $appVersionId);
-  $versionInfo = [
-    "date"     => "",
-    "elapsed"  => 0,
-    "interval" => 0,
-  ];
-} else {
-  $dateTime = "20" . substr($appVersionId, $i + 2);
-  $yyyy     = substr($dateTime, 0, 4);
-  $mm       = substr($dateTime, 4, 2);
-  $dd       = substr($dateTime, 6, 2);
-  $hh       = substr($dateTime, 9, 2);
-  $min      = substr($dateTime, 11, 2);
-  $ss       = substr($dateTime, 13, 2);
 
-  $versionDate     = "$yyyy-$mm-$dd $hh:$min:$ss";
-  $versionDateTime = new DateTime($versionDate, new DateTimeZone('America/Sao_Paulo')); // Assuming UTC-3 is Sao Paulo time
-  $now             = new DateTime('now', new DateTimeZone('UTC'));
+echo "<script>".PHP_EOL;
+echo "const appVersionId = " . json_encode($appVersionId) . ";".PHP_EOL;
+echo "window.appVersionId = appVersionId;".PHP_EOL;
+echo "</script>".PHP_EOL;
 
-  $diff = $now->getTimestamp() - $versionDateTime->getTimestamp();
-
-  $interval = $now->diff($versionDateTime);
-  $ff       = $interval->format('%d.%H:%I:%S');
-  if (str_starts_with($ff, '0.')) {
-    $ff = substr($ff, 2);
-  }
-  $versionInfo = [
-    "date"     => $versionDateTime->format('Y-m-d H:i:s P'),
-    "elapsed"  => (float) $diff,
-    "interval" => $ff,
-  ];
+$fileList = [];
+$glob = glob("../**");
+foreach ($glob as $file) {
+  array_push($fileList, $file);
 }
+error_log("Files in parent directory: " . implode(", ", $fileList));
 
-// dump to error log as raw data
-error_log("App Version ID: " . $appVersionId);
-error_log("Version Date: " . $versionInfo['date']);
-error_log("Elapsed seconds: " . $versionInfo['elapsed']);
-error_log("Interval: " . $versionInfo['interval']);
-echo "<h4>Version Information:</h4>" . PHP_EOL;
-echo "<div>Version Id: " . $appVersionId . PHP_EOL . "</div>" . PHP_EOL;
-echo "<pre>";
-echo "App Version Date: " . $versionInfo['date'] . "\n";
-echo "Now Current Date: " . (new DateTime('now'))->format('Y-m-d H:i:s P') . "\n";
-echo "Elapsed seconds: " . $versionInfo['elapsed'] . PHP_EOL;
-echo "Interval: " . $versionInfo['interval'] . PHP_EOL;
-echo "</pre>";
-echo "<script>" . "setInterval(() => {" . "  const pre = document.querySelector('pre');" . "  const date = pre.textContent.split('App Version Date: ')[1].split('\\n').shift().trim();" . "  const elapsed = Math.floor((Date.now() - new Date(date).getTime()) / 1000);" . "  const interval = new Date(elapsed * 1000).toISOString().substr(11, 8);" . "  pre.textContent = \"App Version Date: \" + date + \"\\nElapsed seconds: \" + elapsed + \"\\nInterval: \" + interval;" . "}, 1000);" . "</script>";
-$routestr = "";
-// list api routes with glob
-foreach (glob("../**") as $file) {
-  if (is_dir($file) && file_exists($file . "/index.php")) {
-    $route = basename($file);
-    if ($route !== "static") {
-      $routestr .= "/" . $route . ",";
+echo "<script>".PHP_EOL;
+echo "const fileList = " . json_encode($fileList) . ";".PHP_EOL;
+echo "window.fileList = fileList;".PHP_EOL;
+echo "</script>".PHP_EOL;
+
+?>
+<script>
+  console.log("App Version ID:", window.appVersionId);
+  console.log("Files in parent directory:", window.fileList);
+</script>
+<pre>
+<div><span>App Version Date:</span><span></span></div>
+<div><span>Now Current Date:</span><span></span></div>
+<div><span>Elapsed seconds:</span><span></span></div>
+<div><span>Interval:</span><span></span></div>
+ </pre>
+<script>
+  function updateTimeInfo() {
+    const appVersionId = window.appVersionId;
+    const fileList = window.fileList;
+
+    // Extract timestamp from appVersionId
+    const timestampMatch = appVersionId.match(/(\d{8}t\d{6})/i);
+    let appVersionDate = "Unknown";
+    if (timestampMatch) {
+      const timestampStr = timestampMatch[1];
+      const year = parseInt(timestampStr.substring(0, 4));
+      const month = parseInt(timestampStr.substring(4, 6)) - 1; // Months are 0-based
+      const day = parseInt(timestampStr.substring(6, 8));
+      const hour = parseInt(timestampStr.substring(9, 11));
+      const minute = parseInt(timestampStr.substring(11, 13));
+      const second = parseInt(timestampStr.substring(13, 15));
+      appVersionDate = new Date(year, month, day, hour, minute, second);
     }
-  } else if (is_dir($file) && $file==="static") {
-    $children = glob($file . "/*");
-    foreach ($children as $child) {
-      $routestr .= "/" . $child . ",";
-    }
-  } else if (is_file($file) && pathinfo($file, PATHINFO_EXTENSION) === "php" && basename($file) !== "index.php") {
-    $route = basename($file, ".php");
-    $routestr .= "/" . $route . ",";
+
+    const now = new Date();
+    const elapsedSeconds = appVersionDate !== "Unknown" ? Math.floor((now - appVersionDate) / 1000) : "Unknown";
+
+    document.querySelector("div:nth-child(1) span:nth-child(2)").textContent = appVersionDate;
+    document.querySelector("div:nth-child(2) span:nth-child(2)").textContent = now.toString();
+    document.querySelector("div:nth-child(3) span:nth-child(2)").textContent = elapsedSeconds;
+    document.querySelector("div:nth-child(4) span:nth-child(2)").textContent = "N/A";
   }
-}
-$list     = explode(",", $routestr);
-echo "<h4>Available Routes:</h4>" . PHP_EOL;
-foreach ($list as $route) {
-  echo "<br><a href='" . htmlentities($route) . "'>" . $route . "</a>" . PHP_EOL;
-}
+
+  updateTimeInfo();
+  setInterval(updateTimeInfo, 1000);
+</script>
--- backend-php/api/flush.php
+++ backend-php/api/flush.php
@@ -1,8 +1,11 @@
 <?php
 
-<?php
-
 $memcache = new Google\AppEngine\Api\Memcache\Memcache();
+
 # reset all
 $memcache->flush();
+
 echo "Flushed all memcache entries.";
+# list all memcache methods
+$methods = get_class_methods($memcache);
+echo "Available Memcache methods: " . implode(", ", $methods);
